7de31d48701668c114b4a0a4054e2438d2bd8ffb
Rafael P R Gouvea <rafael.gouvea@alunos.unis.edu.br>
2022-05-24 17:58:24 -0300

    Alteração na página Produtos
    Editado arquivos HTML, JS e CSS
    para harmonizar os elementos da página

--- js/product.js
+++ js/product.js
@@ -2,31 +2,35 @@ var produtos = [
     {
       imagem: '../images/balm.png',
       nome: 'Balm para barba viking',
-      texto: 'Loção pós barba com hidratação intensiva e aloe vera',
+      texto: ' Loção pós barba com hidratação intensiva e aloe vera.',
     },
     {
       imagem: '../images/pomada.jpg',
-      nome: 'Pomada capilar efeito matte',
-      texto: 'Este produto é perfeito para quem busca eficácia e naturalidade nos cabelos',
+      nome: 'Pomada capilar matte',
+      texto: 'Este produto é perfeito para quem busca eficácia e naturalidade.',
     },
     {
       imagem: '../images/minoxidil.jpg',
       nome: 'Minoxidil Kirkland',
-      texto: 'Indicado para tratamentos contra calvíce e falhas',
+      texto: 'Indicado para tratamentos contra calvíce e falhas.',
     },
     {
       imagem: '../images/kitbarba.jpg',
       nome: 'Kit "Barba de macho"',
-      texto: 'Inclui óleo, shampoo e balm',
-    },
+      texto: 'Inclui óleo, shampoo e balm de extrema qualidade.',
+    }
 ]
   produtos.map((produto) => {
       document.getElementById('produtos').innerHTML += `
         <div class="produto">
             <img src="${produto.imagem}">
+            <div>
             <h2>${produto.nome}</h2>
+            <hr>
             <p>${produto.texto}</p>
-            <button>comprar</button>
+            <br><br><br>
+            <button>Comprar</button>
+            </div>
         </div>
       `
   })
